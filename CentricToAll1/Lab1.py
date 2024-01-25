eaa7e1ab266e86b7b5b2abd6ed06922bff212a84
AbhilashEU <vemulaabhilash03@gmail.com>
2024-01-25 17:50:17 +0530

    My First Commit

---
+++ CentricToAll1/Lab1.py
@@ -0,0 +1,7 @@
+print("First Program - Python print function")
+print("The first program is declared in string with double quote")
+print('Second program : Python print function')
+print('The second program is declared in string with single quote')
+print("print('combination of both quotes')")
+print('print("combination of both quotes")')
+# print('print('Dual single quotes')')  ##"It is not a valid syntax of quoting a string##
